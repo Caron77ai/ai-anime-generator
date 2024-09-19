cf935799819792c47a0e42c94b5fc119f24bf3df
Caron77 <corneyqq@gmail.com>
2024-09-19 21:52:36 +0800

    +1

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -2,23 +2,6 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 
-<<<<<<< HEAD
-const CTAButton = ({ locale }: { locale: any }) => {
-  return (
-<<<<<<< HEAD
-    <Link href="/animegenerator" passHref>
-=======
-    <Link
-      href="https://www.aianimegenerators.com/"
-      target="_blank"
-      rel="noopener noreferrer nofollow"
-    >
->>>>>>> dev
-      <Button
-        variant="default"
-        className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-        aria-label="Get Boilerplate"
-=======
 interface CTAButtonProps {
   locale: {
     title: string;
@@ -42,7 +25,6 @@ export default function CTAButton({ locale }: CTAButtonProps) {
       <button
         onClick={handleSignedInClick}
         className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none"
->>>>>>> dev
       >
         {locale.title}
       </button>
