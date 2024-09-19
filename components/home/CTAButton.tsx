345f25dd5653623b59e761af9a7cdb1940d1a6ef
Caron77 <corneyqq@gmail.com>
2024-09-19 21:54:23 +0800

    +1

--- components/home/CTAButton.tsx
+++ components/home/CTAButton.tsx
@@ -1,26 +1,7 @@
 "use client";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-const CTAButton = ({ locale }: { locale: any }) => {
-  return (
-<<<<<<< HEAD
-    <Link href="/animegenerator" passHref>
-=======
-    <Link
-        href="https://www.aianimegenerators.com/"
-        target="_blank"
-        rel="noopener noreferrer nofollow"
-      >
->>>>>>> dev
-        <Button
-          variant="default"
-          className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white"
-          aria-label="Get Boilerplate"
-=======
->>>>>>> Stashed changes
 interface CTAButtonProps {
   locale: {
     title: string;
@@ -44,25 +25,17 @@ export default function CTAButton({ locale }: CTAButtonProps) {
       <button
         onClick={handleSignedInClick}
         className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none"
-<<<<<<< Updated upstream
       >
         {locale.title}
       </button>
     );
-=======
->>>>>>> dev
-        >
-          {locale.title}
-        </button>
-        );
->>>>>>> Stashed changes
   }
 
-        return (
-        <SignInButton mode="modal">
-          <button className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none">
-            {locale.title}
-          </button>
-        </SignInButton>
-        );
+  return (
+    <SignInButton mode="modal">
+      <button className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-blue-500 hover:bg-blue-600 focus:shadow-outline focus:outline-none">
+        {locale.title}
+      </button>
+    </SignInButton>
+  );
 }
