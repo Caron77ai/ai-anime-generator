e4a4a6c16dc6d762ebdb7030303180c5dafc281f
Caron77 <corneyqq@gmail.com>
2024-10-29 15:39:43 +0800

    +1

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://aianimegenerators.com",
+  siteUrl: 'https://aianimegenerators.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
-};
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: [
+    '/api/*',
+    '/server-sitemap.xml',
+    '/dashboard/*',
+    '/admin/*',
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/*', '/dashboard/*', '/admin/*'],
+      },
+    ],
+  },
+  additionalPaths: async (config) => [
+    { loc: '/', changefreq: 'daily', priority: 1.0 },
+    { loc: '/about', changefreq: 'monthly', priority: 0.8 },
+    { loc: '/pricing', changefreq: 'weekly', priority: 0.8 },
+    { loc: '/generator', changefreq: 'daily', priority: 0.9 },
+    { loc: '/blog', changefreq: 'weekly', priority: 0.8 },
+  ],
+} 
